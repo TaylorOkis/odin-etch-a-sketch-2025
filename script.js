bd161d4336c4de2881770f97b667fa40b46b1ca2
Tammy Taylor <72969270+TaylorOkis@users.noreply.github.com>
2025-05-20 07:58:43 +0100

    Select a random color for each box

--- script.js
+++ script.js
@@ -5,9 +5,17 @@
 const button = document.querySelector("#btn")
 
 button.addEventListener("click", () => {
-    let gridDimension = parseInt(prompt("Enter the square dimension (max: 150)"));
+    let gridDimension = 0;
 
-    drawGrid(gridDimension);
+    while (gridDimension <= 0 || gridDimension > 100) {
+        gridDimension = parseInt(prompt("Enter the square dimension (max: 100)"));
+
+        if (gridDimension > 0 || gridDimension < 100) {
+            drawGrid(gridDimension);
+        } else {
+            alert("Invalid Input. Please enter a number between 1 and 100");
+        }
+    }
 })
 
 function drawGrid(dimension) {
@@ -25,10 +33,13 @@ function drawGrid(dimension) {
             singleBox = document.createElement("div");
 
             singleBox.classList.add("single-box");
-            singleBox.style.cssText = `flex: auto; background-color: ${selectColor()};`;
+            singleBox.style.cssText = "flex: auto; background-color: white;";
+
+            singleBox.addEventListener("mouseover", changeColor);
 
             gridRow.appendChild(singleBox);
         }
+
         gridContainer.appendChild(gridRow);
     }
 }
@@ -41,5 +52,12 @@ function selectColor() {
         colorValues.push(randomValue);
     }
 
-    return `rgb(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]});`
+    return `rgb(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]})`;
+}
+
+function changeColor(event) {
+    if (event.target.style.backgroundColor !== "white") {
+        return;
+    }
+    event.target.style.backgroundColor = selectColor();
 }
